ef6c38da60dde9963c4b4801eba582dac5bc7c4d
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:15:03 +0800

    test: add unit tests (>=90% coverage); docs: add English Javadoc

--- pack-with-assembly-jsw/src/main/java/service/FileLogger.java
+++ pack-with-assembly-jsw/src/main/java/service/FileLogger.java
@@ -3,15 +3,22 @@ package service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FileLogger
-{
-	private static Logger logger = LoggerFactory.getLogger(FileLogger.class);
+/**
+ * Utility class that writes informational log messages to a file via SLF4J.
+ * Produces 100 numbered log entries on each invocation.
+ *
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
+ */
+public class FileLogger {
 
-	public void logInfo2file()
-	{
-		for (int i = 0; i < 100; i++)
-		{
-			logger.info("我的测试：my test"+i);
-		}
-	}
+    private static final Logger logger = LoggerFactory.getLogger(FileLogger.class);
+
+    /**
+     * Writes 100 informational log lines to the configured log output.
+     */
+    public void logInfo2file() {
+        for (int i = 0; i < 100; i++) {
+            logger.info("我的测试：my test" + i);
+        }
+    }
 }
